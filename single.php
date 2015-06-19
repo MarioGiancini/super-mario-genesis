4495b9bfa7e0e53bcdfc53ea8003ce46fd450da4
Mario Giancini <mario.giancini@gmail.com>
2015-06-19 18:16:33 -0400

    Style updates
    Ninja forms styles and utilities

--- single.php
+++ single.php
@@ -6,6 +6,7 @@
 
 remove_action( 'genesis_post_title', 'genesis_do_post_title' );
 remove_action( 'genesis_entry_header', 'genesis_do_post_title' );
+remove_action( 'genesis_entry_header', 'genesis_post_info', 12 );
 remove_action( 'genesis_entry_header', 'genesis_entry_header_markup_open', 5 );
 remove_action( 'genesis_entry_header', 'genesis_entry_header_markup_close', 15 );
 
